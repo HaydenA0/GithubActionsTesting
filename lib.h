f3cccd7327708e8513f38dac5722a95c11128900
HaydenA0 <aregrag2005@gmail.com>
2025-12-28 06:46:33 +0100

    init

---
+++ lib.h
@@ -0,0 +1,1 @@
+int libCustomFunction(void);
---
+++ main.c
@@ -0,0 +1,9 @@
+
+
+#include "lib.h"
+#include <stdio.h>
+int main() {
+  printf("Hello world\n");
+  int output = libCustomFunction();
+  return 1 + output;
+}
